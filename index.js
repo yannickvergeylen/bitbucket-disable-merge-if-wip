fa1a3d3ef7563c5d8b4c22763d60a15c6ed2013b
yakke <yannick.vergeylen@gmail.com>
2019-04-12 13:05:45 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let pullRequestTitle;
+let mergeButton;
+
+const workInProgressMatchers = [
+    '^WIP',
+    '[WIP]'
+];
+
+function checkWipStatus() {
+    if (!pullRequestTitle || mergeButton) {
+        pullRequestTitle = getTitle();
+        mergeButton = getMergeButton();
+    }
+    if (pullRequestTitle && mergeButton) {
+        if (workInProgressMatchers.find(wipMactcher => pullRequestTitle.match(new RegExp(wipMactcher)))) {
+            mergeButton.disabled = true;
+        } else {
+            mergeButton.disabled = false;
+        }
+    }
+}
+
+setTimeout(checkWipStatus, 2000);
+
+
+function getTitle() {
+    return document.getElementsByClassName('pull-request-title')[0].innerText
+}
+
+function getMergeButton() {
+    return document.getElementById('fulfill-pullrequest');
+}
